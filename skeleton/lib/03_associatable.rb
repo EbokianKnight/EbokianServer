be4a7239c2c7f84fa8b0d2d4a30b8da09b8ec196
Adam <ebokianknight@gmail.com>
2016-02-26 18:04:54 -0500

    aaaaahhhhh

--- skeleton/lib/03_associatable.rb
+++ skeleton/lib/03_associatable.rb
@@ -3,23 +3,24 @@ require 'active_support/inflector'
 
 # Phase IIIa
 class AssocOptions < SQLObject
+
   attr_accessor :foreign_key, :class_name, :primary_key
 
   def model_class
-    # ...
+    @class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
     default = {
-      class_name: "#{name.camelcase.singularize}",
+      class_name: "#{name.to_s.camelcase.singularize}",
       primary_key: :id,
-      foreign_key: (name.downcase + "_id").to_sym
+      foreign_key: (name.to_s.downcase + "_id").to_sym
     }
     options = default.merge(options)
     options.each do |ivar, value|
@@ -29,11 +30,12 @@ class BelongsToOptions < AssocOptions
 end
 
 class HasManyOptions < AssocOptions
+
   def initialize(name, self_class_name, options = {})
     default = {
-      class_name: "#{name.camelcase.singularize}",
+      class_name: "#{name.to_s.camelcase.singularize}",
       primary_key: :id,
-      foreign_key: (self_class_name.downcase + "_id").to_sym
+      foreign_key: (self_class_name.to_s.downcase + "_id").to_sym
     }
     options = default.merge(options)
     options.each do |ivar, value|
@@ -45,11 +47,28 @@ end
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    @options = BelongsToOptions.new(name, options)
+    options = BelongsToOptions.new(name, options)
+
+    define_method(name) do
+      klass = options.send :model_class
+      f_key = options.send :foreign_key
+      p_key = options.send :primary_key
+      p p_key
+      p f_key
+      p self.class
+      p klass
+      p klass.where(p_key => send(f_key))
+    end
   end
 
   def has_many(name, options = {})
-    @options = HasManyOptions.new(name, options)
+    options = HasManyOptions.new(name, options)
+    define_method(name) do
+      klass = options.model_class
+      f_key = options.foreign_key
+      p_key = options.primary_key
+      klass.where(f_key => send(p_key))
+    end
   end
 
   def assoc_options
@@ -58,5 +77,5 @@ module Associatable
 end
 
 class SQLObject
-  #extend AssocOptions
+  extend Associatable
 end
