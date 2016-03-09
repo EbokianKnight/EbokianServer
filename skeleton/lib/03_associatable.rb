d02b58723431bbebe8bf173dce1b7b25bc973580
App Academy Student <appacademy@Apps-Mac-mini-94.local>
2016-03-09 11:53:48 -0500

    phase 3 done

--- skeleton/lib/03_associatable.rb
+++ skeleton/lib/03_associatable.rb
@@ -1,9 +1,9 @@
 require_relative '02_searchable'
 require 'active_support/inflector'
+require 'byebug'
 
 # Phase IIIa
 class AssocOptions < SQLObject
-
   attr_accessor :foreign_key, :class_name, :primary_key
 
   def model_class
@@ -30,7 +30,6 @@ class BelongsToOptions < AssocOptions
 end
 
 class HasManyOptions < AssocOptions
-
   def initialize(name, self_class_name, options = {})
     default = {
       class_name: "#{name.to_s.camelcase.singularize}",
@@ -53,26 +52,23 @@ module Associatable
       klass = options.send :model_class
       f_key = options.send :foreign_key
       p_key = options.send :primary_key
-      p p_key
-      p f_key
-      p self.class
-      p klass
-      p klass.where(p_key => send(f_key))
+      object = klass.where(p_key => send(f_key)).first
     end
   end
 
   def has_many(name, options = {})
-    options = HasManyOptions.new(name, options)
+    options = HasManyOptions.new(name, self, options)
+
     define_method(name) do
-      klass = options.model_class
-      f_key = options.foreign_key
-      p_key = options.primary_key
-      klass.where(f_key => send(p_key))
+      klass = options.send :model_class
+      f_key = options.send :foreign_key
+      p_key = options.send :primary_key
+      object = klass.where(f_key => send(p_key))
     end
   end
 
   def assoc_options
-    # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    
   end
 end
 
