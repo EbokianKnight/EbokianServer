eceb4f19bd7adf4f3faec22fc1a5ee222b6321ee
Adam <ebokianknight@gmail.com>
2016-04-15 08:30:36 -0400

    Some Reformatting

--- skeleton/poor_mans_pluralize.rb
+++
@@ -1,8 +0,0 @@
-#String Conversion
-
-
-def poor_mans_pluralize(klass)
-  word = klass.to_s.scan(/[A-Z][a-z]+/).join('_').downcase
-  word[-1].match(/[sxzh]/) ? word << "es" : word << "s"
-  word
-end
